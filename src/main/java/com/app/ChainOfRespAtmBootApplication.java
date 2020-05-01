b0ce2618765c2c0553d286d265118afb34baf9ee
priyanka <chillalpriyu@gmail.com>
2020-05-01 16:47:23 +0530

    adding src code

---
+++ src/main/java/com/app/ChainOfRespAtmBootApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainOfRespAtmBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainOfRespAtmBootApplication.class, args);
+	}
+
+}
