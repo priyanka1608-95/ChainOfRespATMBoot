29d36a0e5ac105017b34c613557604b05acdb67d
priyanka <chillalpriyu@gmail.com>
2020-05-02 11:07:25 +0530

    adding src code

---
+++ src/test/java/com/app/ChainOfRespATMBoot/ChainOfRespAtmBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ChainOfRespATMBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainOfRespAtmBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
