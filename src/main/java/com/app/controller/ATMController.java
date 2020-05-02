ad9ab79072fd172d00a6f8c3022172abcf8684a6
priyanka <chillalpriyu@gmail.com>
2020-05-02 11:07:45 +0530

    adding src code

--- src/main/java/com/app/controller/ATMController.java
+++ src/main/java/com/app/controller/ATMController.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
-@CrossOrigin(allowedHeaders = "*")
 @RequestMapping("/atm")
 public class ATMController
 {
